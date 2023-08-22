68a312e870b56f7666bad124e5321e8b74d8abd4
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-22 13:54:10 +0300

    Mission Passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -4,10 +4,18 @@ def oxford_comma(items):
    elif isinstance(items, list) and len(items) == 2:
     new_list = " and ".join(items)
     return new_list
-       
+   elif isinstance(items, list) and len(items) >= 3:
+    last_item = items[-1]
+    remaining_items = ", " .join(items[:-1])
+    final_list = f"{remaining_items}, and {last_item}"
+
+    return final_list
+
    return items
 
 fruit_list =  oxford_comma(["kiwi"])
 updated_fruit_list = oxford_comma(["kiwi", "durian"])
+newly_updated_fruit_list = oxford_comma(["kiwi", "durian" , "starfruit"])
 print(updated_fruit_list)
 print(fruit_list) 
+print(newly_updated_fruit_list)
